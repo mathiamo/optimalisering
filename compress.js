66312ecb5905e78819b6376d9c26ec0e7216b293
mathiamo <mathiamo@gmail.com>
2018-12-11 20:57:40 +0100

    bildeoptimalisering og brotli

---
+++ compress.js
@@ -0,0 +1,18 @@
+const brotli = require('brotli')
+const fs = require('fs')
+
+const brotliSettings = {
+  extension: 'br',
+  skipLarger: true,
+  mode: 1, // 0 = generic, 1 = text, 2 = font (WOFF2)
+  quality: 10, // 0 - 11,
+  lgwin: 12 // default
+}
+
+fs.readdirSync('./dist/lazy/').forEach(file => {
+  if (file.endsWith('.js') || file.endsWith('.css') || file.endsWith('.html')) {
+    const result = brotli.compress(fs.readFileSync('./dist/lazy/' + file), brotliSettings)
+    console.log(result);
+    fs.writeFileSync('./dist/lazy/' + file + '.br', result)
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lazy';
+
+  imageCollection = [];
+
+constructor() {
+  for (let i = 0; i < 100; i++) {
+      const url = 'http://via.placeholder.com/500x450?text=Image No ' + (i + 1);
+
+      this.imageCollection[i] = {
+        url: url,
+        show: false
+    };
+  }
+}
 }
